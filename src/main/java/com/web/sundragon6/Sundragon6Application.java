5762245bd0eca396893cc9f346d94657a7d35b1a
developer1 <dev1@email.com>
2024-09-10 15:18:04 +0900

    first commit

---
+++ src/main/java/com/web/sundragon6/Sundragon6Application.java
@@ -0,0 +1,13 @@
+package com.web.sundragon6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sundragon6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sundragon6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/sundragon6/Sundragon6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.sundragon6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sundragon6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
